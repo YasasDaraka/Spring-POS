01a8a45b56fce09ebecc3175e2b88fa3cf17e6b6
YasasDaraka <darakayasas@gmail.com>
2024-03-30 22:58:23 +0530

    Update

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/adviser/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.spring.adviser;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
